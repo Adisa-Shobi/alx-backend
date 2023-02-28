07f910fe2b7b2fcbbd39bd8ab4bcbc22dfd8e578
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-02-28 18:04:40 +0000

    Create: '1-app.py' installed

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,10 +3,22 @@
 Basic flask application
 '''
 from flask import Flask, render_template
+from flask_babel import Babel
 
 
 app = Flask(__name__)
+babel = Babel(app)
 
+
+class Config:
+    '''
+    Configuration file for flask
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
 
 @app.route('/', strict_slashes=False)
 def hello_world() -> str:
@@ -16,5 +28,6 @@ def hello_world() -> str:
     return render_template('0-index.html')
 
 
+
 if __name__ == "__main__":
-    app.run(port=5001)
+    app.run()
