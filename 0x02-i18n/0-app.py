f6e52b2949530a605daa6c22acb1f6f5a0069bf6
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-02-28 16:21:12 +0000

    Update: '0-app.py' corrected

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,12 +7,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world():
     '''
     Home route
     '''
-    return render_template('0-index.py')
+    return render_template('0-index.html')
 
 if __name__ == "__main__":
-    app.run(port=5001)
+    app.run()
